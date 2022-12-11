fa4451bd4426ef481d6ae07084b85f073da0ea35
Walkx01 <93958915+Walkx01@users.noreply.github.com>
2022-12-11 17:45:04 -0500

    designed signup screen

--- PersonalRestaurantGuide/screens/User/SignupScreen.js
+++ PersonalRestaurantGuide/screens/User/SignupScreen.js
@@ -1,79 +1,96 @@
-import * as React from 'react'
-import {useAuth, AuthProvider} from "../context/AuthContext";
-import { ScrollView, StyleSheet, Text, View, TextInput, Button, Alert, Image, TouchableHighlight } from 'react-native';
+import * as React from "react";
+import { useAuth, AuthProvider } from "../context/AuthContext";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert,
+  Image,
+  TouchableHighlight,
+} from "react-native";
 const SignupScreen = () => {
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const {dispatch, state} = useAuth(); 
-    return (
-      <View style={styles.container}>
-        <View
-          style={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "flex-end",
-          }}
-        >
-          <Image
-            style={{ width: "30%", height: "30%", resizeMode: "contain" }}
-            source={require("../../assets/restaurant.png")}
-          ></Image>
+  const [username, setUsername] = React.useState("");
+  const [email, setEMail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const { dispatch, state } = useAuth();
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "flex-end",
+        }}
+      >
+        <Image
+          style={{ width: "30%", height: "30%", resizeMode: "contain" }}
+          source={require("../../assets/restaurant.png")}
+        ></Image>
 
-          <Text style={styles.lightText}> Personal Restaurant Guide</Text>
-        </View>
-        <View
-          style={{
-            flex: 3,
-            borderColor: "red",
-            paddingHorizontal: "10%",
-            justifyContent: "center",
-          }}
-        >
-          <Text style={{ fontSize: 25, fontWeight: "bold" }}>Sign in</Text>
-          <Text style={styles.lightText}>
-            {" "}
-            HI there ! Nice to see you again.
-          </Text>
-          <Text style={styles.labels}> Username</Text>
-          <TextInput
-            style={styles.inputs}
-            value={username}
-            onChangeText={setUsername}
-          />
-          <Text style={styles.labels}> Password</Text>
-          <TextInput
-            style={styles.inputs}
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-          />
-
-          <TouchableHighlight
-            style={[styles.button]}
-            activeOpacity={0.6}
-            underlayColor="#DDDDDD"
-            onPress={() => {
-              dispatch({ type: "login" });
-              console.log(state);
-            }}
-          >
-            <Text style={styles.text}> Sign in</Text>
-          </TouchableHighlight>
-        </View>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            alignItems: "baseline",
-            paddingHorizontal: "15%",
-          }}
-        >
-          <Text style={{ color: "#abb5be" }}> Don't have an account ?</Text>
-          <Button color={"#f85f6a"} title=" Sign Up" />
-        </View>
+        <Text style={styles.lightText}> Personal Restaurant Guide</Text>
       </View>
-    );
-}
+      <View
+        style={{
+          flex: 3,
+          borderColor: "red",
+          paddingHorizontal: "10%",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ fontSize: 25, fontWeight: "bold" }}>Sign Up</Text>
+        <Text style={styles.lightText}>
+          {" "}
+          Find the best restaurants near you
+        </Text>
+        <Text style={styles.labels}> Username</Text>
+        <TextInput
+          style={styles.inputs}
+          value={username}
+          onChangeText={setUsername}
+        />
+        <Text style={styles.labels}> Email</Text>
+        <TextInput
+          style={styles.inputs}
+          value={email}
+          onChangeText={setEMail}
+        />
+        <Text style={styles.labels}> Password</Text>
+        <TextInput
+          style={styles.inputs}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+
+        <TouchableHighlight
+          style={[styles.button]}
+          activeOpacity={0.6}
+          underlayColor="#DDDDDD"
+          onPress={() => {
+            dispatch({ type: "login" });
+            console.log(state);
+          }}
+        >
+          <Text style={styles.text}> Sign Up</Text>
+        </TouchableHighlight>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          alignItems: "baseline",
+          paddingHorizontal: "15%",
+        }}
+      >
+        <Text style={{ color: "#abb5be" }}> already have an account ?</Text>
+        <Button color={"#f85f6a"} title=" Sign In" />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
